9831ab20b8751555c0f2f70804605dac75c4a106
Yash <yashatkulwar2000@gmail.com>
2022-10-03 12:43:28 +0530

    added Basic Java Programs

---
+++ PositiveOrNegative.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Practice;
+import java.util.Scanner;
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x > 0){
+            System.out.println("Positive Number");
+        }   else{
+            System.out.println("Negative Number");
+        }
+
+
+
+    }
+}
+
